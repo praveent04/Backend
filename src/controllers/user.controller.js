2d4c2bb4090dac27b492febab6838415d9ff0789
Praveen Tiwari <praveentiwari29004@gmail.com>
2025-05-10 16:23:48 +0530

    implemented login facility

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -4,6 +4,23 @@ import {User} from "../models/user.model.js"
 import {uploadOnCloudinary} from "../utils/cloudinary.js"
 import { ApiResponse } from "../utils/ApiResponse.js";
 
+
+
+const generateAccessAndRefreshToken = async(userId) => {
+    try {
+        const user = await User.findById(userId)
+        const accessToken = user.generateAccessToken()
+        const refreshToken = user.generateRefreshToken()
+
+        user.refreshToken = refreshToken
+        await user.save({validateBeforeSave: false})
+
+        return {accessToken, refreshToken}
+    } catch (error) {
+        throw new ApiError(500, "something went wrong while generating refresh and access tokens: ", error)
+    }
+}
+
 const registerUser = asyncHandler( async(req, res) => {
      // get user detail
      // validation of info - not empty
@@ -37,7 +54,16 @@ const registerUser = asyncHandler( async(req, res) => {
       console.log(req.files)
 
     const avatarLocalPath = req.files?.avatar[0]?.path
-    const coverImageLocalPath = req.files?.coverImage[0]?.path;
+    //const coverImageLocalPath = req.files?.coverImage[0]?.path;
+
+    let coverImageLocalPath
+    // checking if cover image is present or not
+    if(req.files && Array.isArray(req.files.coverImage) && req.files.coverImage.length >0 ){
+        coverImageLocalPath = req.files.coverImage[0].path
+    }
+    // checking if avatar is present or not 
+    // both ways of checking are different in syntax but working is same 
+    // just avatar is necessary whereas coverimage is not that's why both checks are different
 
     if(!avatarLocalPath){
         throw new ApiError(400, "Avatar file is required")
@@ -73,6 +99,60 @@ const registerUser = asyncHandler( async(req, res) => {
 
 })
 
+
+const loginUser = asyncHandler(async ( req, res) =>{
+    // req body ->data
+    // login on username email
+    // find user
+    // password check
+    // access and refresh token
+    // send cookies
+
+    const {email, username, password} = req.body
+
+    if(!username || !email){
+        throw new ApiError(400, "username or email is required")
+    }
+
+    const user =  await User.findOne({
+        $or: [{username}, {email}]
+    })
+
+    if(!user){
+        throw new ApiError(404, "USer does not exist")
+    }
+
+    const isPasswordValid = await user.isPasswordCorrect(password)
+
+    if(!isPasswordValid){
+        throw new ApiError(401, "invalid password")
+    }
+
+    const {accessToken, refreshToken} = await generateAccessAndRefreshToken(user._id)
+
+    const loggedInUser = await User.findById(used._id).select("-password -refreshToken ")
+    
+    const options = {
+        httpOnly: true,
+        secure: true
+    }
+
+    return res.status(200).
+    cookie("accessToken", accessToken, options)
+    .cookie("refreshToken", refreshToken, options)
+    .json(
+        new ApiResponse(200, {
+            user: loggedInUser,accessToken,refreshToken
+        },
+        "User Logged in Successfully"
+    )
+    )
+})
+
+const logoutUser = asyncHandler( async ( req,res) =>{
+    
+})
 export {
     registerUser,
+    loginUser
 }
